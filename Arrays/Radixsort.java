b957707ba5a9e02441d5761de692ba7a13538539
Deepanshi <56440331+gargdeepanshi@users.noreply.github.com>
2021-05-05 17:06:33 +0530

    Add files via upload

---
+++ Arrays/Radixsort.java
@@ -0,0 +1,54 @@
+package pepcoding;
+
+import java.util.Scanner;
+
+public class Radixsort {
+    static void print(int arr[], int n)
+    {
+        for (int i = 0; i < n; i++)
+            System.out.print(arr[i] + " ");
+    }
+    static int getMax(int arr[], int n)
+    {
+        int mx = arr[0];
+        for (int i = 1; i < n; i++)
+            if (arr[i] > mx)
+                mx = arr[i];
+        return mx;
+    }
+    static void radixsort(int arr[], int n) {
+        int m = getMax(arr, n);
+        for(int exp=1;m/exp>0;exp*=10)
+            countSort(arr, n, exp);
+    }
+
+    private static void countSort(int[] arr, int n, int exp)
+    {
+        int output[]=new int[n];
+        int count[]=new int[10];
+        for(int i=0;i<n;i++)
+            count[(arr[i]/exp)%10]++;
+
+        for (int i = 1; i < 10; i++)
+            count[i] += count[i - 1];
+
+        for(int i=n-1;i>=0;i--)
+        {
+            output[count[(arr[i]/exp)%10]-1]=arr[i];
+            count[(arr[i]/exp) % 10]--;
+        }
+        for (int i = 0; i < n; i++)
+            arr[i] = output[i];
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+            arr[i]=s.nextInt();
+        radixsort(arr,n);
+        print(arr,n);
+    }
+}
